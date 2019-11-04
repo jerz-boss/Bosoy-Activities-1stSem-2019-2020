15966078c75f3a30def02bd808709c7780b08a29
jerz-boss <56668104+jerz-boss@users.noreply.github.com>
2019-11-04 11:53:57 +0800

    Add files via upload

---
+++ Initials.java
@@ -0,0 +1,10 @@
+public class Initials{
+      public static void main(String [] args){
+      
+      System.out.println("     J BBBB");
+      System.out.println("     J B  BB");
+      System.out.println("     J BBBB");
+      System.out.println("J    J B  BB");
+      System.out.println("JJJJJJ BBBB");
+      
+      }}
---
+++ MyInfo.java
@@ -0,0 +1,10 @@
+public class MyInfo{
+      public static void main(String [] args){
+      
+      System.out.println("Name:        Jerson c. Bosoy");
+      System.out.println("Course:      BSIT-Major in Network and Security");
+      System.out.println("Year:        1st Yr.");
+      System.out.println("Address:     Lower Pob. Sablan, Benguet");
+      System.out.println("Adjective:   Simple");
+      
+      }}
